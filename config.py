e4c1def260a906cef49eeb0da88b7bfbc1b6feda
strands <strands@scitos>
2017-08-01 11:11:39 +0100

    Nav Debugged

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ MENU = [{"name": "Red Sweet", "image": "red_sweet.jpg"},
         {"name": "Blue Sweet", "image": "blue_sweet.jpg"}
         ]
 
-HUB = 'WayPoint1'  # Kitchen/Food source
-NUMBER_OF_WAYPOINTS = 14
+HUB = 'WayPoint20'  # Kitchen/Food source
+NUMBER_OF_WAYPOINTS = 30
 ONE_MACHINE = True  # Only working with LUCIE, no login for admin features
 PIN = "1111"  # Not so secure Login password.
 WONDERING_MODE = True  # Randomly visit WayPoints when looking for an order
-TWITTER = True  # Display link to twitter or not. Use with selfie program.
-STRANDS_UI_URL = "127.0.0.1:5000"  # The URL for strands__ui
+TWITTER = False  # Display link to twitter or not. Use with selfie program.
+STRANDS_UI_URL = "127.0.0.1:8090"  # The URL for strands__ui
