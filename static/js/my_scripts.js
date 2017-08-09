3f9c157212112a975351c78f221db39d74d0c902
PaulBrownMagic <pbrownpaul@gmail.com>
2017-08-09 13:34:32 +0100

    Continual Timeouts

--- static/js/my_scripts.js
+++ static/js/my_scripts.js
@@ -1,3 +1,7 @@
+var navsocket;
+var contentsocket;
+var ordersocket;
+
 $(document).ready(function() {
     // Open Websockets
     navsocket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + '/nav');
@@ -5,10 +9,10 @@ $(document).ready(function() {
     ordersocket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + '/orders');
 
     contentsocket.on('new_content', function(content){
-        $('#main_content').html(content)
+        $('#main_content').html(content);
 
         // When button id=decline is clicked:
-        $('#decline').click(function(event){
+        $('#decline').click(function(){
             $("#declineModal").modal("show");
             // Time out Modal that pops up, it'll hide, robot is already moving away.
             window.setTimeout(function () {
@@ -65,8 +69,8 @@ function nav_to(destination){
 
 // Update values in menu, used in "home.html"
 function change(diff, id){
-    var formid = "[id='f "+ id + "']"
-    var showid = "[id='s "+ id + "']"
+    var formid = "[id='f "+ id + "']";
+    var showid = "[id='s "+ id + "']";
     var num = parseInt($(formid).val(), 10);
     if (num + diff <= 5 && num + diff >= 0 ){
         num = num + diff;
@@ -78,7 +82,7 @@ function change(diff, id){
 
 // Disable order button when all values are 0, used in "home.html"
 function can_order() {
-    var max = $(":input[type=number]").map(function(){ return this.value }).get().sort().reverse()[0]
+    var max = $(":input[type=number]").map(function(){ return this.value }).get().sort().reverse()[0];
     if (max == 0){  // max ordered is 0, therefore all values are 0
         $('#order').attr('disabled', true);
     }
