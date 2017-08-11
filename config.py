f5440f0d4a533e7e24a73694fa276e5978329a9a
strands <strands@scitos>
2017-08-11 15:23:23 +0100

    Longroom Config

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ MENU = [{"name": "Red Sweet", "image": "red_sweet.jpg"},
         {"name": "Blue Sweet", "image": "blue_sweet.jpg"}
         ]
 
-HUB = 'WayPoint20'  # Kitchen/Food source
-NUMBER_OF_WAYPOINTS = 30
+HUB = 'WayPoint1'  # Kitchen/Food source
+NUMBER_OF_WAYPOINTS = 25
 ONE_MACHINE = True  # Only working with LUCIE, no login for admin features
 PIN = "1111"  # Not so secure Login password.
 WONDERING_MODE = True  # Randomly visit WayPoints when looking for an order
-TWITTER = False  # Display link to twitter or not. Use with selfie program.
+TWITTER = True  # Display link to twitter or not. Use with selfie program.
 STRANDS_UI_URL = "127.0.0.1:8090"  # The URL for strands__ui
