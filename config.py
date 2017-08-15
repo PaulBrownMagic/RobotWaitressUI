ad15dfbec0e5dff23315bac933c171e5c224dbe6
strands <strands@scitos>
2017-08-15 14:14:44 +0100

    Single Timeout

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ MENU = [{"name": "Red Sweet", "image": "red_sweet.jpg"},
         {"name": "Blue Sweet", "image": "blue_sweet.jpg"}
         ]
 HUB = 'WayPoint1'  # Kitchen/Food source
-<<<<<<< HEAD
 NUMBER_OF_WAYPOINTS = 30
-ONE_MACHINE = False  # Only working with LUCIE, no login for admin features
+ONE_MACHINE = True  # Only working with LUCIE, no login for admin features
 PIN = "1111"  # Not so secure Login password.
 # NAVIGATING_MODE Options: "HUB" "RANDOM" "PATROL" "RANDOM_PATROL"
 NAVIGATING_MODE = "RANDOM_PATROL"
-TWITTER = True  # Display link to twitter or not. Use with selfie program.
+TWITTER = False  # Display link to twitter or not. Use with selfie program.
